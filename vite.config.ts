31705b9bf006d34baf5d6e283bee13ad3a43eb90
seaders <seaders69@gmail.com>
2025-01-28 22:48:06 +0000

    Multiple plugins env vars

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,30 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, PluginOption } from "vite";
 // import { intlayerPlugin } from "vite-intlayer";
 
-export const intlayerPlugin = (): PluginOption => ({
+const customPlugin = (): PluginOption => ({
   name: "custom-plugin",
 
   config: () => {
     process.env = {
-      // ...process.env,
+      ...process.env,
+      VITE_CUSTOM_VAR: "A CUSTOM VAR",
+    };
+  },
+});
 
-      // VITE_CUSTOM_VAR2: "A CUSTOM VAR2",
-      VITE_CUSTOM_VAR: "A CUSTOM VAR3",
+const intlayerPlugin = (): PluginOption => ({
+  name: "custom-plugin",
+
+  config: () => {
+    process.env = {
+      ...process.env,
+      VITE_INTLAYER_VAR: "A CUSTOM VAR3",
     };
   },
 });
 
 export default defineConfig({
-  plugins: [react(), intlayerPlugin()],
+  plugins: [react(), intlayerPlugin(), customPlugin()],
   server: {
     open: true,
   },
