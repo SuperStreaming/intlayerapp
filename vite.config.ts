b0dc9852ea73263206cc780b070dbf40ab2ff919
seaders <seaders69@gmail.com>
2025-01-29 10:24:01 +0000

    Remove intlayer, add more edge case (wrong?) plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,57 @@
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 import { defineConfig, PluginOption } from "vite";
-// import { intlayerPlugin } from "vite-intlayer";
 
-const customPlugin = (): PluginOption => ({
-  name: "custom-plugin",
+export const plugin1 = (): PluginOption => ({
+  name: "plugin1",
 
   config: () => {
     process.env = {
       ...process.env,
       VITE_CUSTOM_VAR: "A CUSTOM VAR",
+      VITE_A_OVERRIDED_VAR: "OVERRIDED PLUGIN 1",
     };
   },
 });
 
-const intlayerPlugin = (): PluginOption => ({
-  name: "custom-plugin",
+export const plugin2 = (): PluginOption => ({
+  name: "plugin2",
 
   config: () => {
     process.env = {
-      ...process.env,
-      VITE_INTLAYER_VAR: "A CUSTOM VAR3",
+      // ...process.env,
+      VITE_CUSTOM_VAR2: "A CUSTOM VAR 2",
+      VITE_A_OVERRIDED_VAR: "OVERRIDED PLUGIN 2",
     };
+  },
+});
+
+export const plugin3 = (): PluginOption => ({
+  name: "plugin3",
+
+  config: () => {
+    process.env.VITE_CUSTOM_VAR3 = "A CUSTOM VAR 3";
+    process.env.VITE_A_OVERRIDED_VAR = "OVERRIDED PLUGIN 3";
+  },
+});
+
+export const brokenPlugin = (): PluginOption => ({
+  name: "brokenPlugin",
+
+  config: (config) => {
+    dotenv.config({
+      path: [
+        `.env.${config.mode}.local`,
+        `.env.${config.mode}`,
+        ".env.local",
+        ".env",
+      ],
+    });
   },
 });
 
 export default defineConfig({
-  plugins: [react(), intlayerPlugin(), customPlugin()],
+  plugins: [react(), plugin1(), brokenPlugin(), plugin2(), plugin3()],
   server: {
     open: true,
   },
